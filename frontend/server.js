fff73df02815a223541c053e86ecd50a4dda2f92
Navaratnarajah <navani.rajah@Imp.net>
2019-06-05 14:10:57 +0100

    frontend

---
+++ frontend/server.js
@@ -0,0 +1,22 @@
+const http = require("http");
+    const fs = require("fs");
+
+    http.createServer((req, res) => {
+        if (req.url === "/rando") {
+            const toReq = {
+                hostname: "openshift",
+                port: 8000,
+                path: "/rando",
+                method: "GET"
+            };
+            const backendReq = http.request(toReq, resp => {
+                let body = "";
+                resp.on("data", chunk => (body += chunk));
+                resp.on("end", () => res.end(body));
+            });
+            backendReq.end();
+        } else {
+            const myhtml = fs.readFileSync("./wowgreatcss.html");
+            res.end(myhtml);
+        }
+    }).listen(8000);
