1621ffc9a0acf046caa2061b74551fa31fcf80e8
Navaratnarajah <navani.rajah@Imp.net>
2019-06-05 13:54:35 +0100

    hiiiiiiiiiii

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const MongoClient = require("mongodb").MongoClient;
+    const http = require("http");
+    const url = "mongodb://admin:pass@mongodb-34-centos7:27017";
+
+    MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+        http.createServer((req, res) => {
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            if (req.url === "/rando" && req.method === "GET") {
+                const db = client.db("admin");
+                const collection = db.collection("test");
+                collection.find({}).toArray((err, docs) => {
+                    const toSend = docs[Math.floor(Math.random() * docs.length)].name;
+                    res.end(toSend);
+                });
+            }
+        }).listen(8000);
+    });
